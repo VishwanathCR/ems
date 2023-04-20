4441a390fe81a8548c944332022206ae5def07ef
Vishwanath R <vishwanath.cr.13@gmail.com>
2023-04-20 10:00:54 +0530

    JWT user details

---
+++ src/main/java/com/vishkode/ems/auth/enums/Role.java
@@ -0,0 +1,6 @@
+package com.vishkode.ems.auth.enums;
+
+public enum Role {
+    ADMIN,
+    USER
+}
---
+++ src/main/java/com/vishkode/ems/auth/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.vishkode.ems.auth.repository;
+
+import com.vishkode.ems.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
